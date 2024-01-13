4b90db389d08290d892eeba30ca71e0b5b39f9f9
glax <johanna@bernloehr.eu>
2024-01-13 23:54:15 +0100

    Added list of shockerIds to abstract class Shocker

--- OpenCS2hock/Shocker.cs
+++ OpenCS2hock/Shocker.cs
@@ -2,13 +2,20 @@
 
 public abstract class Shocker
 {
-    public string ApiKey, Endpoint;
-    public enum ControlAction { Beep, Vibrate, Shock }
-    public abstract void Control(ControlAction action, byte intensity, short duration);
+    protected readonly HttpClient HttpClient;
+    protected readonly string ApiKey;
+    protected readonly string Endpoint;
+    protected string[] ShockerIds;
 
-    public Shocker(string endpoint, string apiKey)
+    public enum ControlAction { Beep, Vibrate, Shock }
+
+    public abstract void Control(ControlAction action, byte intensity, short duration, string? shockerId = null);
+
+    protected Shocker(string endpoint, string apiKey, string[] shockerIds)
     {
         this.Endpoint = endpoint;
         this.ApiKey = apiKey;
+        this.HttpClient = new HttpClient();
+        this.ShockerIds = shockerIds;
     }
 }
