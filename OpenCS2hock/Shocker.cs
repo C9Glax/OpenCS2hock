18543a14e5193fdfff8000d76afa18c61cea5f9f
glax <johanna@bernloehr.eu>
2024-01-13 21:44:04 +0100

    abstract class Shocker

---
+++ OpenCS2hock/Shocker.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenCS2hock;
+
+public abstract class Shocker
+{
+    public string ApiKey, Endpoint;
+    public enum ControlAction { Beep, Vibrate, Shock }
+    public abstract void Control(ControlAction action, byte intensity, short duration);
+
+    public Shocker(string endpoint, string apiKey)
+    {
+        this.Endpoint = endpoint;
+        this.ApiKey = apiKey;
+    }
+}
