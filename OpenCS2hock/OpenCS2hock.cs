bf2acf6835d924f0a22464f61f1e611528211a0b
glax <johanna@bernloehr.eu>
2024-01-13 21:43:32 +0100

    Main Method

---
+++ OpenCS2hock/OpenCS2hock.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenCS2hock;
+
+public class OpenCS2hock
+{
+    private GSIServer GSIServer { get; init; }
+    public OpenCS2hock()
+    {
+        GSIServer gsiServer;
+        Installer.InstallGsi();
+        this.GSIServer = new GSIServer(3000);
+        
+        this.GSIServer.Dispose();
+    }
+}
--- OpenCS2hock/Program.cs
+++ OpenCS2hock/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        new OpenCS2hock();
     }
 }
